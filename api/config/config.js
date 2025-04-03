52ebff204a70f4981cfa241106a5db55362494ab
KevinGalvisSA <kesangari201006@gmail.com>
2025-04-03 14:07:06 -0500

    chore: :hammer: Implementation of environment variables.
    Configuration of environment variables for database management.

--- api/config/config.js
+++ api/config/config.js
@@ -1,14 +1,16 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  logging: false, // Desactiva logs de SQL en consola (opcional)
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // Necesario en Railway para evitar errores SSL
-    },
-  },
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+  }
+);
 
 module.exports = sequelize;
